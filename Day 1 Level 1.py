eb93d35303e1e3252330f26e7d004dbaca35cbd5
UP240077 <up240077@alumnos.upa.edu.mx>
2025-06-05 10:37:41 -0600

    creacion de primer archivo para repositoriO

---
+++ Day 1 Level 1.py
@@ -0,0 +1,29 @@
+##Ej.-1
+import math
+
+
+print (4+3)
+print (1000-200)
+print (2*314)
+print (10%4)
+print (1748/392)
+print (2**3)
+print (91//22)
+print ("hola, soy angel")
+print ("Mi mama se llama laura")
+print ("Vivo en Aguascalientes")
+print ("Estoy disfrutando los 30 dias de python")
+#EJ 3.-
+print (int(19.4))
+print(float(19.3))
+print (complex(10.23))
+print (type ("Asabeneh"))
+print (list ([1,2,3]))
+print (type ({"name: Asabeneh"}))
+print (type({9.8, 3.14, 2.7}))
+print (type((9.8, 3.14, 2.7)))
+print ("Distancia eudiciana")
+
+#Calcula la distancia eudiciana
+d= math.sqrt((10-2)**2 + (8-3)**2)
+print (d)
---
+++ hello.py
@@ -0,0 +1,21 @@
+#Este codigo es de prueba
+print ("Hello world")
+n= 2+3 
+print (n)
+m= 2-3
+print (m)
+b= 2*5
+print (b)
+bn= 100/2
+print (bn)
+c= 3%4 
+print (c)
+Bm= 4 // 3 
+print (Bm)
+bj= 4**3
+print (bj)
+bi= 4//41
+print bi
+#Bien hecho, ahora sabes usar el python shell 
+#Mi nombre es Angel, soy de la familia correa vivo en mexico y estoy disfrutando de los 30 dias de python
+
